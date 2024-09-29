fe522485149b9745044071baf664188696d5e1fc
professortX <kevintirta814@gmail.com>
2024-09-29 23:35:47 +0800

    feat: gas fees rebates

--- prover/circuits/circuit_test.go
+++ prover/circuits/circuit_test.go
@@ -1,64 +1,68 @@
-package age
+package gasFeesRebate
 
 import (
-	"context"
-	"fmt"
 	"math/big"
 	"testing"
 
 	"github.com/brevis-network/brevis-sdk/sdk"
 	"github.com/brevis-network/brevis-sdk/test"
 	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/core/types"
-	"github.com/ethereum/go-ethereum/ethclient"
 )
 
 func TestCircuit(t *testing.T) {
 	app, err := sdk.NewBrevisApp()
 	check(err)
-	ec, err := ethclient.Dial("https://eth.llamarpc.com")
-	check(err)
 
-	txHash := common.HexToHash(
-		"4a18c7762036fcd4016d9ba74b40a8c3614adf9b6f7c6439c4675f9e828211c8")
+	GAS_FEE_TOKEN := common.HexToAddress("0xdac17f958d2ee523a2206206994597c13d831ec7")
 
-	fmt.Sprintf("txHash: %s", txHash)
-	tx, _, err := ec.TransactionByHash(context.Background(), txHash)
-	check(err)
-	receipt, err := ec.TransactionReceipt(context.Background(), txHash)
-	check(err)
-	from, err := types.Sender(types.NewLondonSigner(tx.ChainId()), tx)
-	check(err)
-
-	gtc := big.NewInt(0)
-	gasFeeCap := big.NewInt(0)
-	if tx.Type() == types.LegacyTxType {
-		gtc = tx.GasPrice()
-	} else {
-		gtc = tx.GasTipCap()
-		gasFeeCap = tx.GasFeeCap()
-	}
-
-	app.AddTransaction(sdk.TransactionData{
-		Hash:                txHash,
-		ChainId:             tx.ChainId(),
-		BlockNum:            receipt.BlockNumber,
-		Nonce:               tx.Nonce(),
-		GasTipCapOrGasPrice: gtc,
-		GasFeeCap:           gasFeeCap,
-		GasLimit:            tx.Gas(),
-		From:                from,
-		To:                  *tx.To(),
-		Value:               tx.Value(),
+	app.AddStorage(sdk.StorageData{
+		BlockNum: big.NewInt(19290434),
+		Address:  GAS_FEE_TOKEN,
+		Slot:     common.HexToHash("0x568f97cb8c4c4a5582f76b76203c3168e6b403a6cad2536bcda6c6a37564ab52"),
+		Value:    common.HexToHash("0x00000000000000000000000000000000000000000000000000000005a3d8ce7b"),
 	})
 
-	guest := &AppCircuit{}
-	guestAssignment := &AppCircuit{}
+	app.AddStorage(sdk.StorageData{
+		BlockNum: big.NewInt(19525436),
+		Address:  GAS_FEE_TOKEN,
+		Slot:     common.HexToHash("0x568f97cb8c4c4a5582f76b76203c3168e6b403a6cad2536bcda6c6a37564ab52"),
+		Value:    common.HexToHash("0x00000000000000000000000000000000000000000000000000000005a3d8ce7b"),
+	})
 
-	circuitInput, err := app.BuildCircuitInput(guest)
+	app.AddStorage(sdk.StorageData{
+		BlockNum: big.NewInt(19525436),
+		Address:  GAS_FEE_TOKEN,
+		Slot:     common.HexToHash("0x568f97cb8c4c4a5582f76b76203c3168e6b403a6cad2536bcda6c6a37564ab52"),
+		Value:    common.HexToHash("0x0000000000000000000000000000000000000000000000000000000660eafbb9"),
+	})
+
+	app.AddStorage(sdk.StorageData{
+		BlockNum: big.NewInt(19525436),
+		Address:  GAS_FEE_TOKEN,
+		Slot:     common.HexToHash("0x568f97cb8c4c4a5582f76b76203c3168e6b403a6cad2536bcda6c6a37564ab52"),
+		Value:    common.HexToHash("0x0000000000000000000000000000000000000000000000000000000210ezzb59"),
+	})
+
+	app.AddStorage(sdk.StorageData{
+		BlockNum: big.NewInt(19525436),
+		Address:  GAS_FEE_TOKEN,
+		Slot:     common.HexToHash("0x568f97cb8c4c4a5582f76b76203c3168e6b403a6cad2536bcda6c6a37564ab52"),
+		Value:    common.HexToHash("0x000000000000000000000000000000000000000000000000000000043af52942"),
+	})
+
+	appCircuit := &AppCircuit{}
+	appCircuitAssignment := &AppCircuit{}
+
+	in, err := app.BuildCircuitInput(appCircuitAssignment)
 	check(err)
 
-	test.ProverSucceeded(t, guest, guestAssignment, circuitInput)
+	///////////////////////////////////////////////////////////////////////////////
+	// Testing
+	///////////////////////////////////////////////////////////////////////////////
+
+	// Use the test package to check if the circuit can be solved using the given
+	// assignment
+	test.ProverSucceeded(t, appCircuit, appCircuitAssignment, in)
 }
 
 func check(err error) {
